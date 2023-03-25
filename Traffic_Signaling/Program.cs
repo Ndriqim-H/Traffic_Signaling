733807f3fd8edf76e5686f5290935b35480f3ae0
Ndriçim_Halili <ndriqim_@hotmail.com>
2023-03-25 17:59:05 +0100

    Brainstorming

--- Traffic_Signaling/Program.cs
+++ Traffic_Signaling/Program.cs
@@ -1,127 +1,145 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program
+namespace Traffic_Signaling
 {
-    public static void Main(string[] args)
+    class Program
     {
-        var input = File.ReadAllLines(Directory.GetCurrentDirectory() + "\\a_an_example.in.txt");
-
-        // Parse input
-        var parameters = input[0].Split(' ');
-        var D = int.Parse(parameters[0]);
-        var I = int.Parse(parameters[1]);
-        var S = int.Parse(parameters[2]);
-        var V = int.Parse(parameters[3]);
-        var F = int.Parse(parameters[4]);
-
-        //Fill the intersections
-        List<int> intersections = new();
-        for (int i = 1; i <= I + 1; i++)
+        public static void Main(string[] args)
         {
-            intersections.Add(int.Parse(input[i].Split(' ')[1]));            
-        }
-        List<Street> streets = new();
-        for (int i = 1; i <= S; i++)
-        {
-            var arr = input[i].Split(' ');
-            streets.Add(new Street()
+            var input = File.ReadAllLines(Directory.GetCurrentDirectory() + "\\a_an_example.in.txt");
+
+            // Parse input
+            var parameters = input[0].Split(' ');
+            //Simulation duration
+            var D = int.Parse(parameters[0]);
+            //Number of intersections
+            var I = int.Parse(parameters[1]);
+            //Number of streets
+            var S = int.Parse(parameters[2]);
+            //Number of cars
+            var V = int.Parse(parameters[3]);
+            //Number of points for reaching the destination on time
+            var F = int.Parse(parameters[4]);
+
+            
+            //Parse all the streets
+            List<Street> streets = new();
+            for (int i = 1; i <= S; i++)
             {
-                Id = i - 1,
-                Starts = int.Parse(arr[0]),
-                Ends = int.Parse(arr[1]),
-                Name = arr[2],
-                Time = int.Parse(arr[3])
-            });
-        }
-
-        
-        //List<string> streets = new();
-        //for (int i = 0; i < S; i++)
-        //{
-        //    streets.Add(intersections[2][i]);
-        //}
-
-        //Fill the paths for the cars
-        List<string[]> paths = new();
-        List<Path> pathsList = new();
-        for (int i = 1 + S; i < input.Length; i++)
-        {
-            var arr = input[i].Split(' ');
-            string[] arr1 = new string[arr.Length - 1];
-            for (int j = 1; j < arr.Length - 1; j++)
-            {
-                arr1[j-1] = arr[j];
-                
-                pathsList.Add(new Path()
+                streets.Add(new Street()
                 {
-                    Order = j,
-                    StreetId = streets.Where(t => t.Name == arr[j]).Select(t=>t.Id).First()
+                    Id = i,
+                    Starts = int.Parse(input[i].Split(' ')[0]),
+                    Ends = int.Parse(input[i].Split(' ')[1]),
+                    Name = input[i].Split(' ')[2],
+                    Time = int.Parse(input[i].Split(' ')[3])
                 });
             }
-            paths.Add(arr1);
 
-        }
-        
-        //Connect all the paths to the intersections
-        List<InterSectionPath> allPathIntersections = new();
-        for (int i = 0; i < paths.Count; i++)
-        {
-            allPathIntersections.Add(new InterSectionPath()
+            //Create intersections
+            List<Intersection> intersections = new();
+            for (int i = 0; i < I; i++)
             {
-                PathId = i,
-                Streets = streets.Where(t => paths[i].Contains(t.Name)).ToList(),
-            });
-        }
-
-
-        List<int> notUsedIntersections = new();
-
-        //Find all intersections which are not in any path
-        List<int> allEnds = new();
-        for (int i = 0; i < allPathIntersections.Count; i++)
-        {
-            allEnds.AddRange(allPathIntersections[i].Streets.Select(t => t.Ends));
-        }
-        //Remove unused intersections
-        notUsedIntersections.AddRange(intersections.Where(t => !allEnds.Contains(t)));
-
-        intersections = allEnds.Where(t => !notUsedIntersections.Contains(t)).ToList();
-        
-        //Find duplicates in intersections and save them
-        
-        var importatIntersections = intersections.GroupBy(x => x)
-              //.Where(g => g.Count() > 1)
-              .Select(y => (y.Key, y.Count()))
-              .ToList();
-        var trivialIntersections = intersections.Where(t => !importatIntersections
-        .Where(x=>x.Item2 > 1)
-        .Select(x=>x.Key)
-                    .Contains(t)).ToList();
-
-        //Find the paths whose intersections are unique, i.e. intersections where only 1 car passes through
-        string x = "";
-
-        for (int i = 0; i < allPathIntersections.Count; i++)
-        {
-            x += allPathIntersections[i].Streets.Where(t => trivialIntersections.Contains(t.Ends))
-                .Select(t => t.Name).FirstOrDefault() + ", ";
-        }
-
-        List<IntersectionSchedule> y = new();
-        for (int i = 0; i < allPathIntersections.Count; i++)
-        {
-            y.Add(allPathIntersections[i].Streets.Where(t => trivialIntersections.Contains(t.Ends))
-                .Select(t => new IntersectionSchedule()
+                intersections.Add(new Intersection()
                 {
-                    Interval = 2,
-                    IncomingNo = 1,
-                    StreetName = t.Name
-                }).First());
+                    Id = i,
+                    Streets = streets.Where(t=>t.Ends == i).ToList(),
+                });
+            }
+
+            //Create car paths
+            List<Path> paths = new();
+            //List<Intersection> intersectionsInPath = new();
+
+            for (int i = S + 1; i < V + S + 1; i++)
+            {
+                var arr = input[i].Split(' ');
+                //List<string> pathStreets = new();
+                //for (int j = 0; j < pathStreets.Count; j++)
+                //{
+                //    pathStreets.Add(arr[j + 1]);
+                //}
+
+                List<Street> streetsInPath = new();
+                //Get all the streets from the path except the last one
+                for (int j = 1; j < arr.Length - 1; j++)
+                {
+                    var street = streets.Where(t => t.Name == arr[j]).First();
+                    streetsInPath.Add(new Street()
+                    {
+                        Name = arr[j],
+                        Id = street.Id,
+                        Ends = street.Ends,
+                        Starts = street.Starts,
+                        Time = street.Time,
+                    });
+                    
+
+                    
+                }
+
+                paths.Add(new Path()
+                {
+                    Id = i - (S + 1),
+                    NumberOfIntersections = int.Parse(arr[0]),
+                    Streets = streetsInPath,
+                    Destination = arr[arr.Length - 1]
+                });
+
+                
+
+
+            }
+
+
+            
+            for (int i = 0; i < paths.Count; i++)
+            {
+                List<Intersection> intersactionsInPath = new();
+                
+                for (int j = 0; j < paths[i].Streets.Count; j++)
+                {
+                    
+                    Intersection intersection = intersections.Where(t=>t.Id == paths[i].Streets[j].Ends).First();
+                    intersactionsInPath.Add(intersection);
+                }
+                paths[i].Intersections = intersactionsInPath;
+                
+            }
+            //for (int i = 0; i < intersectionsInPath.Count; i++)
+            //{
+            //    if (intersectionsInPath[i].Streets.Count == 1)
+            //    {
+            //        intersectionsInPath[i].GreenInterval = 1;
+            //        intersectionsInPath[i].GreenTimeForStreets = new List<int> { 1 };
+            //    }
+            //    else
+            //    {
+            //        intersectionsInPath[i].GreenInterval = intersectionsInPath[i].Streets.Count;
+            //        intersectionsInPath[i].GreenTimeForStreets = new();
+            //        for (int j = 0; j < intersectionsInPath[i].Streets.Count; j++)
+            //        {
+            //            intersectionsInPath[i].GreenTimeForStreets.Add(1);
+            //        }
+            //    }
+            //}
+            Console.WriteLine("Hello World!");
+
         }
 
+    }
 
-        
-        Console.WriteLine(intersections);
+    class Intersection
+    {
+        public int Id { get; set; }
+        public List<Street> Streets { get; set; }
+        public int GreenInterval { get; set; }
+        public List<int> GreenTimeForStreets { get; set; }
     }
 
     class Street
@@ -132,31 +150,12 @@ class Program
         public string Name { get; set; }
         public int Time { get; set; }
     }
-
-    class InterSectionPath
-    {
-        public List<Street> Streets { get; set; }
-        public int PathId { get; set; }
-    }
-
     class Path
     {
-        public int StreetId  { get; set; }
-        public int Order { get; set; }
+        public int Id { get; set; }
+        public int NumberOfIntersections { get; set; }
+        public List<Intersection> Intersections { get; set; }
+        public List<Street> Streets { get; set; }
+        public string Destination { get; set; }
     }
-
-    class IntersectionSchedule
-    {
-        public int IncomingNo { get; set; }
-        public int Interval { get; set; }
-        public string StreetName { get; set; }
-        public List<FinalStreet> Street { get; set; }
-    }
-
-    class FinalStreet
-    {
-        public string Name { get; set; }
-        public int GreenTime { get; set; }
-    }
-    
 }
---
+++ Traffic_Signaling/Program1.cs
@@ -0,0 +1,162 @@
+﻿using System.Linq;
+
+class Program1
+{
+    public static void Main1(string[] args)
+    {
+        var input = File.ReadAllLines(Directory.GetCurrentDirectory() + "\\a_an_example.in.txt");
+
+        // Parse input
+        var parameters = input[0].Split(' ');
+        var D = int.Parse(parameters[0]);
+        var I = int.Parse(parameters[1]);
+        var S = int.Parse(parameters[2]);
+        var V = int.Parse(parameters[3]);
+        var F = int.Parse(parameters[4]);
+
+        //Fill the intersections
+        List<int> intersections = new();
+        for (int i = 1; i <= I + 1; i++)
+        {
+            intersections.Add(int.Parse(input[i].Split(' ')[1]));            
+        }
+        List<Street> streets = new();
+        for (int i = 1; i <= S; i++)
+        {
+            var arr = input[i].Split(' ');
+            streets.Add(new Street()
+            {
+                Id = i - 1,
+                Starts = int.Parse(arr[0]),
+                Ends = int.Parse(arr[1]),
+                Name = arr[2],
+                Time = int.Parse(arr[3])
+            });
+        }
+
+        
+        //List<string> streets = new();
+        //for (int i = 0; i < S; i++)
+        //{
+        //    streets.Add(intersections[2][i]);
+        //}
+
+        //Fill the paths for the cars
+        List<string[]> paths = new();
+        List<Path> pathsList = new();
+        for (int i = 1 + S; i < input.Length; i++)
+        {
+            var arr = input[i].Split(' ');
+            string[] arr1 = new string[arr.Length - 1];
+            for (int j = 1; j < arr.Length - 1; j++)
+            {
+                arr1[j-1] = arr[j];
+                
+                pathsList.Add(new Path()
+                {
+                    Order = j,
+                    StreetId = streets.Where(t => t.Name == arr[j]).Select(t=>t.Id).First()
+                });
+            }
+            paths.Add(arr1);
+
+        }
+        
+        //Connect all the paths to the intersections
+        List<InterSectionPath> allPathIntersections = new();
+        for (int i = 0; i < paths.Count; i++)
+        {
+            allPathIntersections.Add(new InterSectionPath()
+            {
+                PathId = i,
+                Streets = streets.Where(t => paths[i].Contains(t.Name)).ToList(),
+            });
+        }
+
+
+        List<int> notUsedIntersections = new();
+
+        //Find all intersections which are not in any path
+        List<int> allEnds = new();
+        for (int i = 0; i < allPathIntersections.Count; i++)
+        {
+            allEnds.AddRange(allPathIntersections[i].Streets.Select(t => t.Ends));
+        }
+        //Remove unused intersections
+        notUsedIntersections.AddRange(intersections.Where(t => !allEnds.Contains(t)));
+
+        intersections = allEnds.Where(t => !notUsedIntersections.Contains(t)).ToList();
+        
+        //Find duplicates in intersections and save them
+        
+        var importatIntersections = intersections.GroupBy(x => x)
+              //.Where(g => g.Count() > 1)
+              .Select(y => (y.Key, y.Count()))
+              .ToList();
+        var trivialIntersections = intersections.Where(t => !importatIntersections
+        .Where(x=>x.Item2 > 1)
+        .Select(x=>x.Key)
+                    .Contains(t)).ToList();
+
+        //Find the paths whose intersections are unique, i.e. intersections where only 1 car passes through
+        string x = "";
+
+        for (int i = 0; i < allPathIntersections.Count; i++)
+        {
+            x += allPathIntersections[i].Streets.Where(t => trivialIntersections.Contains(t.Ends))
+                .Select(t => t.Name).FirstOrDefault() + ", ";
+        }
+
+        List<IntersectionSchedule> y = new();
+        for (int i = 0; i < allPathIntersections.Count; i++)
+        {
+            y.Add(allPathIntersections[i].Streets.Where(t => trivialIntersections.Contains(t.Ends))
+                .Select(t => new IntersectionSchedule()
+                {
+                    Interval = 2,
+                    IncomingNo = 1,
+                    StreetName = t.Name
+                }).First());
+        }
+
+
+        
+        Console.WriteLine(intersections);
+    }
+
+    class Street
+    {
+        public int Id { get; set; }
+        public int Starts { get; set; }
+        public int Ends { get; set; }
+        public string Name { get; set; }
+        public int Time { get; set; }
+    }
+
+    class InterSectionPath
+    {
+        public List<Street> Streets { get; set; }
+        public int PathId { get; set; }
+    }
+
+    class Path
+    {
+        public int StreetId  { get; set; }
+        public int Order { get; set; }
+    }
+
+    class IntersectionSchedule
+    {
+        public int IncomingNo { get; set; }
+        public int Interval { get; set; }
+        public string StreetName { get; set; }
+        public List<FinalStreet> Street { get; set; }
+    }
+
+    class FinalStreet
+    {
+        public string Name { get; set; }
+        public int GreenTime { get; set; }
+    }
+    
+}
